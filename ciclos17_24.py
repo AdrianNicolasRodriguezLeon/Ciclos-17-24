0609b32e96fed55d87b5c9a081fd1857d049c634
Adrián Nicolás Rodríguez León <arodriguezle@unal.edu.co>
2025-05-05 08:06:18 -0500

    punto 18

---
+++ ciclos17_24.py
@@ -0,0 +1,14 @@
+
+### TERCERA ENTREGRA ###
+#  Estudiante: Adrian Nicolas Estiven Rodriguez Leon
+numero = int(input("Ingresa un número entero: "))
+i = 1
+suma = 0
+
+# Sumar los 100 números siguientes
+while i <= 100:
+    suma += numero + i
+    i += 1
+
+# Mostrar el resultado
+print("La suma de los 100 números siguientes a", numero,  "es:", suma)
