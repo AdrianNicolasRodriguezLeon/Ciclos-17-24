af8710add8ea6a683ee58d7db930047330e86288
Adrián Nicolás Rodríguez León <arodriguezle@unal.edu.co>
2025-05-05 08:29:48 -0500

    punto 19

--- ciclos17_24.py
+++ ciclos17_24.py
@@ -4,11 +4,20 @@
 numero = int(input("Ingresa un número entero: "))
 i = 1
 suma = 0
-
-# Sumar los 100 números siguientes
 while i <= 100:
     suma += numero + i
     i += 1
-
-# Mostrar el resultado
 print("La suma de los 100 números siguientes a", numero,  "es:", suma)
+print ("  ")
+print("19. Conversion")
+print(" ")
+tasa_conversion = 1.14  
+opcion = int(input("Escribe 1 para convertir euros a dólares: "))
+while opcion == 1:
+    euros = float(input("Ingresa la cantidad en euros: "))
+    dolar = euros * tasa_conversion
+    print ( "los " , euros,  "euros equivalen a", dolar, "dólares.")
+    opcion = int(input("¿Deseas convertir otra cantidad? escribe 1 si quieres continuar: "))
+    if opcion != 1:
+       print("Opción no válida.")
+
